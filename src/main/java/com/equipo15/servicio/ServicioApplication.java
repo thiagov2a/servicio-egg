15d6ee9ad15fbce12d6d5021510e16476af66e62
Thiago Vildosa <tvildosa@gmail.com>
2023-11-17 20:47:13 -0300

    initialize

---
+++ src/main/java/com/equipo15/servicio/ServicioApplication.java
@@ -0,0 +1,13 @@
+package com.equipo15.servicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipon/servicio/ServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipon.servicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
