7e88ee34907b959c190a4d65fb72c97675fb28c7
Alveiro Izarra <alviza1209@gmail.com>
2023-11-21 22:10:43 -0300

    Merge origin/main into main
    Conflicts:
    	src/main/java/com/equipo15/servicio/SeguridadWeb.java
    	src/main/java/com/equipo15/servicio/enumeraciones/Rol.java
    	src/main/java/com/equipo15/servicio/excepciones/MiException.java

--- src/main/java/com/equipo15/servicio/excepciones/MiException.java
+++ src/main/java/com/equipo15/servicio/excepciones/MiException.java
@@ -11,12 +11,4 @@ public class MiException extends Exception{
     public MiException(String msg) {
         super(msg);
     }   
-package com.equipo15.servicio.excepciones;
-
-public class MiException extends Exception {
-
-    public MiException(String mensaje) {
-        super(mensaje);
-    }
-
 }
