f720edc0a61292a9951609ce6477406c7cb020ad
Thiago Vildosa <tvildosa@gmail.com>
2023-11-29 23:23:03 -0300

    update

--- src/main/resources/static/JS/Scripts.js
+++
@@ -1,9 +0,0 @@
-// JavaScript para mostrar/ocultar el menú desplegable al hacer clic en el botón en el navbar
-document.addEventListener('DOMContentLoaded', function () {
-  var dropdownBtn = document.querySelector('.nav_btn');
-  var dropdownContent = document.querySelector('.nav_dropdown-content');
-
-  dropdownBtn.addEventListener('click', function () {
-    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
-  });
-});
