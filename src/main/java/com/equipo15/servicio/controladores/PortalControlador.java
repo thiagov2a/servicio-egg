e0c75b1579d1b1e90629abd76f4445fd0ff7eb3c
Carla <carlitaormina93@gmail.com>
2023-11-30 21:06:50 -0300

    creacion css

--- src/main/java/com/equipo15/servicio/controladores/PortalControlador.java
+++ src/main/java/com/equipo15/servicio/controladores/PortalControlador.java
@@ -65,6 +65,8 @@ public class PortalControlador {
             @RequestParam(required = false) Integer precioPorHora,
             @RequestParam(required = false) Integer calificacion,
             @RequestParam(required = false) String idServicio,
+            
+            
             ModelMap modelo) {
         try {
             if (esProveedor) {
